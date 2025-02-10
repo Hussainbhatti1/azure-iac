1d77f19bb3943d3694cd2d3660a77e24caafb518
Hussain Bhatti <hussain.nuces@gmail.com>
2025-02-10 18:17:33 -0500

    update jenkins file

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "my-terraform-rg"
+}
+
+variable "location" {
+  description = "Azure region (e.g., Canada Central)"
+  type        = string
+  default     = "Canada Central"
+}
+
 variable "subscription_id" {
   description = "The Azure Subscription ID"
   type        = string
